4fbdf2b9f98f27604ef341b7d51a7d5390ba3791
GORDOMOSNTRO <w.nattytdread@gmail.com>
2024-08-15 04:11:44 -0300

    testando

--- src/js/script.js
+++ src/js/script.js
@@ -6,19 +6,19 @@ function enviar() {
     alert("Por favor, preencha os campos.");
   } else {
     lvl = parseInt(lvl, 10);
-    if (lvl >= 1 && lvl <= 5) {
-      alert(`${nickname}, sua patente é Prata`);
-    }
-    if (lvl >= 6 && lvl <= 10) {
-      alert(`${nickname}, sua patente é Ak`);
-    }
-    if (lvl >= 11 && lvl <= 15) {
-      alert(`${nickname}, sua patente é Ak Cruzada`);
-    }
-    if (lvl >= 16 && lvl < 19) {
-      alert(`${nickname}, sua patente é Supremo`);
-    } else if (lvl >= 19) {
-      alert(`${nickname}, sua patente é Global`);
-    }
+  }
+  if (lvl >= 1 && lvl <= 5) {
+    alert(`${nickname}, sua patente é Prata`);
+  }
+  if (lvl >= 6 && lvl <= 10) {
+    alert(`${nickname}, sua patente é Ak`);
+  }
+  if (lvl >= 11 && lvl <= 15) {
+    alert(`${nickname}, sua patente é Ak Cruzada`);
+  }
+  if (lvl >= 16 && lvl < 19) {
+    alert(`${nickname}, sua patente é Supremo`);
+  } else if (lvl >= 19) {
+    alert(`${nickname}, sua patente é Global`);
   }
 }
